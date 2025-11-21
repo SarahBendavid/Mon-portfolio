1d6f0994d805f98a05e0831d915bc4bd8708b918
Sarah Bendavid <sarah.laura.bendavid@gmail.com>
2025-11-21 10:47:43 +0200

    Modif main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'   // ✅ Router
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -8,8 +8,8 @@ import './i18n.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
