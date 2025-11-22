6a420eff12eca15f2cde24ef172e5a6c0e4ca12b
Sarah Bendavid <sarah.laura.bendavid@gmail.com>
2025-11-22 15:01:31 +0200

    safe css

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: true,
+    postcss: {},
+  },
+  build: {
+    minify: "esbuild",
+  },
   base: "/Mon-portfolio/",
 });
